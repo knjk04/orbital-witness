37f3dc908d7e5524bada65bf8b8bbd9f5f3678fb
Karan Kumar <karan@karankumar.com>
2023-06-12 16:26:05 +0100

    refactor: extract read JSON to function

--- main.py
+++ main.py
@@ -9,55 +9,58 @@ import sys
 # 4. Lessee’s title: The unique identifier for the sub lease property
 # 5. (Optional) notes
 
-def print_hi():
-    # read JSON
-    # with open('schedule_of_notices_of_lease_examples.json', 'r') as file:
+def read_input():
     with open('sample.json', 'r') as file:
         json_data = json.load(file)[0]
-        print(json.dumps(json_data, indent=4))
+        # print(json.dumps(json_data, indent=4))
         # print(json.dumps(json_data['leaseschedule']['scheduleEntry'], indent=4))
+        return json_data
 
 
-        print('\n*********\n')
+def main():
+    json_data = read_input()
+    # print('\n*********\n')
 
-        for entries in json_data['leaseschedule']['scheduleEntry']:
-            reg_date = ''
-            property_desc = ''
-            lease_date = ''
-            lessee_title = ''
-            note = ''
+    # TODO: inline read_input
+    for entries in json_data['leaseschedule']['scheduleEntry']:
+        reg_date = ''
+        property_desc = ''
+        lease_date = ''
+        lessee_title = ''
+        note = ''
 
-            for entry_text in entries['entryText']:
-                print(entry_text)
-                # split on at 2 spaces (1 space is insufficient)
-                split = entry_text.split("  ")
-                # remove empty strings
-                split = list(filter(None, split))
-                print('Split: ', split)
+        for entry_text in entries['entryText']:
+            print(entry_text)
+            # split on at 2 spaces (1 space is insufficient)
+            split = entry_text.split("  ")
+            # remove empty strings
+            split = list(filter(None, split))
+            print('Split: ', split)
 
-                if len(split) >= 1:
-                    if is_note(split[0]):
-                        # TODO: handle multiple notes by splitting them with a character
-                        note += split[0]
-                    else:
-                        reg_date += ' ' + split[0]
-                print('  -> Reg date:', reg_date)
+            if len(split) >= 1:
+                if is_note(split[0]):
+                    # TODO: handle multiple notes by splitting them with a character
+                    note += split[0]
+                else:
+                    reg_date += ' ' + split[0]
+            print('  -> Reg date:', reg_date)
 
-                if len(split) >= 2:
-                    property_desc = split[1]
-                print('  -> Property description:', property_desc)
+            if len(split) >= 2:
+                property_desc = split[1]
+            print('  -> Property description:', property_desc)
 
-                if len(split) >= 3:
-                    lease_date = split[2]
-                print('  -> Lease date:', lease_date)
+            if len(split) >= 3:
+                lease_date = split[2]
+            print('  -> Lease date:', lease_date)
 
-                if len(split) >= 4:
-                    lessee_title = split[3]
-                print('  -> Lessee title:', lessee_title)
+            if len(split) >= 4:
+                lessee_title = split[3]
+            print('  -> Lessee title:', lessee_title)
 
-            print('  -> Note:', note)
+        print('  -> Note:', note)
 
-            print('\n')
+        print('\n')
+        # TODO: output JSON
 
 
 def is_date(s: str) -> bool:
@@ -76,7 +79,6 @@ def is_date(s: str) -> bool:
         return False
 
 
-
 def is_note(s: str) -> bool:
     # assumes we are interested in all notes
     return s.startswith("NOTE")
@@ -84,6 +86,6 @@ def is_note(s: str) -> bool:
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-    print_hi()
+    main()
 
 
