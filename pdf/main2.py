675b7e522df85723c7b437a20538ac96c90376c1
Karan Kumar <karan@karankumar.com>
2023-06-13 20:07:35 +0100

    Read non-note data

--- pdf/main2.py
+++ pdf/main2.py
@@ -4,10 +4,6 @@ import pandas as pd
 import tabula
 
 
-def get_position_of_date(s: str) -> (int, int):
-    return re.search(r"\d\d\.\d\d\.\d\d\d\d", s).span()
-
-
 class ScheduleOfNoticesOfLeases:
     def __init__(self, reg_date: str, property_desc: str, lease_date: str, lessee_title: str, note: str = None):
         self.reg_date = reg_date
@@ -17,67 +13,62 @@ class ScheduleOfNoticesOfLeases:
         self.note = note
 
 
+def value_present(s) -> bool:
+    return str(s) != "nan"
+
+
 print("\n******************\n")
 file = "Official_Copy_Register.pdf"
 # Without specifying the header option as None, the first row will be treated as a header row
-df = tabula.read_pdf(file, pages="all", pandas_options={'header': None})
-print(df[2].to_string(index=False))
+df_list: list[pd.DataFrame] = tabula.read_pdf(file, pages="all", pandas_options={'header': None})
+print(df_list[2].to_string(index=False))
 
-num_rows = len(df[2])
+num_rows = len(df_list[2])
 print(f"\nNumber of rows: {num_rows}")
 
 print("\n******************\n")
 
-# Read a row of a dataframe
-print("\nFirst row:\n")
-df2 = df[2]
-df2.columns = ["index", "reg_date", "property_desc", "lease_date", "lessee_title"]
-row = df2.iloc[0]
-print("Index: ", row.loc["index"])
-print("Reg date: ", row.loc["reg_date"])
-print("Property desc: ", row.loc["property_desc"])
-print("Lease date: ", row.loc["lease_date"])
-print("Lessee title: ", row.loc["lessee_title"])
+notices_of_leases: [ScheduleOfNoticesOfLeases] = []
+# Start from 2 as this is where the schedule of notices is
+row_num = 1
+for rows in df_list[2:]:
+    print("First set of rows:\n")
+    print(rows)
+    rows.columns = ["index", "reg_date", "property_desc", "lease_date", "lessee_title"]
 
+    default_val = float('-inf')
+    index = default_val
+    for i in range(len(rows)):
+        row = rows.iloc[i]
 
+        if value_present(str(row.loc["index"])):
+            if index != default_val:
+                # Then index was updated, so this row represents a new entry
+                # This only needs to apply for the first time it is set to 1. On subsequent occasions, e.g. ...
+                # index = 2.0, this will still work.
+                # Save the previous entry
+                notices_of_leases.append(ScheduleOfNoticesOfLeases(
+                    reg_date=reg_date, property_desc=property_desc, lease_date=lease_date, lessee_title=lessee_title
+                ))
+            # TODO: stop accessing value from series multiple times
+            # start of a new row, so add
+            index = row.loc["index"]
+            reg_date = row.loc["reg_date"]
+            property_desc = row.loc["property_desc"]
+            lease_date = row.loc["lease_date"]
+            lessee_title = row.loc["lessee_title"]
+        else:
+            # continuation of row, so append
+            reg_date += " " + row.loc["reg_date"] if value_present(row.loc["reg_date"]) else ""
+            property_desc += " " + row.loc["property_desc"] if value_present(row.loc["property_desc"]) else ""
+            lease_date += " " + row.loc["lease_date"] if value_present(row.loc["lease_date"]) else ""
+            lessee_title += " " + row.loc["lessee_title"] if value_present(row.loc["lessee_title"]) else ""
 
-MANUAL_SPLIT = False
-if MANUAL_SPLIT:
-    print("\n******************\n")
-    for line in df[2].to_string(index=False).split("\n"):
-        print(f"\nLine: {line}")
-        split = line.split("  ")
-        split = list(filter(None, split))  # remove empty strings
-        index, reg_date, lease_date = split
+        print(f"Row {row_num} Index: ", index)
+        print(f"Row {row_num} Reg date: ", reg_date)
+        print(f"Row {row_num} Property desc: ", property_desc)
+        print(f"Row {row_num} Lease date: ", lease_date)
+        print(f"Row {row_num} Lessee title: ", lessee_title, "\n")
+        row_num += 1
 
-        index = index.lstrip()
-        reg_date = reg_date.lstrip()
-        lease_date = lease_date.lstrip()
-
-        # Extract the property description from the registration date
-        # RegEx used instead of split() because only space separates the two columns, but the
-        # property description field itself may contain spaces
-        _, reg_date_end_idx = get_position_of_date(reg_date)
-        property_desc = reg_date[reg_date_end_idx:].lstrip()
-        # remove property description from reg date
-        reg_date = reg_date[:reg_date_end_idx]
-
-        # extract lessee title from lease date
-        _, lessee_title_start_pos = get_position_of_date(lease_date)
-        lessee_title = lease_date[lessee_title_start_pos:].lstrip()
-        # remove lessee title from date of lease
-        lease_date = lease_date[:lessee_title_start_pos]
-
-        x=3  # TODO: remove
-        # index, reg_date, property_desc, date_of_lease, lessee_title = line.split("  ")
-        print(f"Index: {index}")
-        print(f"Reg date: {reg_date}")
-        print(f"Property description: {property_desc}")
-        print(f"Date of lease: {lease_date}")
-        print(f"Lessee title: {lessee_title}")
-        schedule = ScheduleOfNoticesOfLeases(
-            reg_date=reg_date, property_desc=property_desc, lease_date=lease_date, lessee_title=lessee_title
-        )
-        x=4  # TODO: remove
-
-
+        x = 5
